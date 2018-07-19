80d57a700d16b8583da5f951e31e416b3edbee1d
errorr404 <dkbishwash@gmail.com>
2018-07-19 16:40:38 +0530

    adding the yarg library to simplified read input commands

--- app.js
+++ app.js
@@ -2,24 +2,33 @@ console.log('Starting App');
 // here require is used for importing the built-in module from node.
 const fs = require('fs'); // import the file system module.
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js') // import the file from same directory
 
+const argv = yargs.argv;
 
-var command = process.argv[2]; // here to take command we write in command prompt node app.js command
+//var command = process.argv[2]; // here to take command we write in command prompt node app.js command
+var command = argv._[0];
 console.log('command:',command);
+console.log('process',process.argv);
+console.log('yargs',argv);
 if(command==='add')
 {
   console.log('adding the notes');
+  notes.addNote(argv.title,argv.body);
 }
 else if(command==='list')
 {
   console.log('listing all the notes');
+  notes.getAll();
 } else if(command==='read')
 {
   console.log('fetching all the notes');
+  notes.getNote(argv.title);
 } else if(command==='remove')
 {
   console.log('removing the notes');
+  nodes.removeNote(argv.title)
 }
 else console.log('command not recognized');
--- notes.js
+++ notes.js
@@ -1,7 +1,24 @@
 console.log('starting notes.js');
 // console.log(module);
 //here we export a const age by module
-module.exports.addNote = ()=>{
-  console.log('Add note Here');
-  return 'Notes added';
-}
+var addNote = (title,body) =>{
+  console.log("Adding note",title,body);
+};
+
+var getAll =() =>{
+  console.log('getting all the content');
+};
+
+var readNote = (title) => {
+  console.log('title to read',title);
+};
+
+var removeNote = (title) => {
+  console.log('title to remove',title);
+};
+module.exports = {
+  addNote,
+  getAll,
+  readNote,
+  removeNote
+};
