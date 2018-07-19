77b8be9de8e912c2c01f7da8cc561e8680537b3c
errorr404 <dkbishwash@gmail.com>
2018-07-19 15:58:42 +0530

    getting user input from command line

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 console.log('Starting App');
 // here require is used for importing the built-in module from node.
 const fs = require('fs'); // import the file system module.
-const os = require('os'); // import the os module to grab username.
-const notes = require('./notes.js') // import the file from same directory
 const _ = require('lodash');
 
-console.log(_.isString(true)); // isString method return the true or false value.
-console.log(_.isString('true'));
-
-var filteredArray = _.uniq(['Dixit',1,2,'Dixit',2,3,5,1,5]);
-console.log(filteredArray);
+const notes = require('./notes.js') // import the file from same directory
 
 
-
-// const val = notes.addNum(5,7);
-// console.log(val);
-
-
-
-
-// const user = os.userInfo();
-// //console.log(user);
-//
-// // appendFileSync is used for make a file in the directory.
-// // here it makes a greeting.txt file in the directory with some messege which is passed.
-// fs.appendFileSync('greeting.txt',`Hello ${user.username} and you are ${notes.age} old !`);
+var command = process.argv[2]; // here to take command we write in command prompt node app.js command
+console.log('command:',command);
+if(command==='add')
+{
+  console.log('adding the notes');
+}
+else if(command==='list')
+{
+  console.log('listing all the notes');
+} else if(command==='read')
+{
+  console.log('fetching all the notes');
+} else if(command==='remove')
+{
+  console.log('removing the notes');
+}
+else console.log('command not recognized');
--- notes.js
+++ notes.js
@@ -1,11 +1,7 @@
 console.log('starting notes.js');
 // console.log(module);
 //here we export a const age by module
-
-// module.exports.age =25;
-
-// here we make a function for adding two numbers and export that
-module.exports.addNum = (a,b)=>{
-  console.log('returnd sum is a+b');
-  return a+b;
+module.exports.addNote = ()=>{
+  console.log('Add note Here');
+  return 'Notes added';
 }
